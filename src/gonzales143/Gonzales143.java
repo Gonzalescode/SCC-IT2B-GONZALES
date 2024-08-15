05508e1b5e6f8ecbcb9b8efaeb57dd6d6cd6c9c2
Gonzalescode <gonzalesjonathan703@gmail.com>
2024-08-15 10:12:25 +0800

    my first repo

---
+++ src/gonzales143/Gonzales143.java
@@ -0,0 +1,18 @@
+
+package gonzales143;
+
+
+public class Gonzales143 {
+    public static void main(string[] args){
+     
+     string fname;
+     int curr,byear;
+     
+     system.out.print("Enter your name: ");
+     fname = input.nextLine();
+     system.out.print("Enter birth date: ");   
+     byear = input.nextLine();
+     
+    } 
+  
+}
